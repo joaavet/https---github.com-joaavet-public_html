ab1b8d16d4ae33bf432308d0cfd1675a42e21541
Joao Vitor Esteves Tavares <joao.tavares@prefeitura.net>
2021-06-29 13:47:18 -0300

    muitas

--- controllers/MedicosController.php
+++ controllers/MedicosController.php
@@ -2,6 +2,9 @@
 
 namespace app\controllers;
 use app\models\Medicos;
+use Da\QrCode\Qrcode;
+use Da\QrCode\Format\MeCardFormat; 
+use yii; 
 
 class MedicosController extends \yii\web\Controller
 {
@@ -18,8 +21,27 @@ class MedicosController extends \yii\web\Controller
     {
 
         $medico =  Medicos::findOne($id);
+        $format = new MeCardFormat();
+        $format-
+$format->firstName = $medico->Nome;
+$format->email =  $medico->Email;
+$format->note =  $medico->CRM;
+$format->address =  $medico->Endereco . ', '. $medico->Bairro;
+$format->url = 'https://'.medico->site;
 
+
+        $qrCode=(new Qrcode($format))
+        ->setSize(250)
+        ->setMargin(5)
+        useForegroundColor(0 , 0 , 0);
+
+        // new wp can display the qrcode in many ways
+        // saving the result ip a file
+
+    $qrCode->writeFile(Yii::detAlias('@web') . 'img/{$medico->$Medico_id}-{$medico->Nome}.png'); // writer defauts to PNG when none is specifed
         return $this->render('view',['medico' => $medico]);
+        'medico' ->$medico,
+        'qrcode' ->$qrcode
     }
 
 }
